54fe09b283e372d0818c8840c2713ff816d59b0c
msmi3128 <msmi3128@gmail.com>
2016-04-23 14:30:49 -0500

    Change routing

--- frontend/source_js/app.js
+++ frontend/source_js/app.js
@@ -2,23 +2,23 @@ var app = angular.module('mp4', ['ngRoute', 'mp4Controllers', 'mp4Services']);
 
 app.config(['$routeProvider', function($routeProvider) {
   $routeProvider.
-    when('/firstview', {
-    templateUrl: 'partials/firstview.html',
-    controller: 'FirstController'
+    when('/home', {
+    templateUrl: 'partials/home.html',
+    controller: 'HomeController'
   }).
-  when('/secondview', {
-    templateUrl: 'partials/secondview.html',
-    controller: 'SecondController'
+  when('/create', {
+    templateUrl: 'partials/create.html',
+    controller: 'CreateController'
   }).
-  when('/settings', {
-    templateUrl: 'partials/settings.html',
-    controller: 'SettingsController'
+  when('/myschedules', {
+    templateUrl: 'partials/myschedules.html',
+    controller: 'MySchedulesController'
   }).
-  when('/llamalist', {
-    templateUrl: 'partials/llamalist.html',
-    controller: 'LlamaListController'
+  when('/scheduledetail', {
+    templateUrl: 'partials/scheduledetail.html',
+    controller: 'ScheduleDetailController'
   }).
   otherwise({
-    redirectTo: '/settings'
+    redirectTo: '/home'
   });
 }]);
